ca43f4c3f1c6aeafc03e0e0739617ddfdc25260e
Japheth0214 <japarcillians@gmail,com>
2023-10-31 20:45:57 +0800

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,9 +20,9 @@ export const signin = async (req, res, next) => {
 const { email, password } = req.body;
 try {
     const validUser = await User.findOne ({ email });
-    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    if (!validUser) return next(errorHandler(404, "We couldn't find an account that matches your email."));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+    if (!validPassword) return next(errorHandler(401, "The password you've entered is incorrect. Please try again"));
     const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
     const { password: pass, ...rest } = validUser._doc;
     res

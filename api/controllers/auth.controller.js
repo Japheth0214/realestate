845bb9c8e89e97c9542cc73d0559604ccf2cc169
Japheth0214 <japarcillians@gmail,com>
2023-11-07 22:29:53 +0800

    Add Sign Out function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,35 +1,32 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from "../utils/error.js";
+import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
-
 export const signup = async (req, res, next) => {
-const { username, email, password } = req.body;
-const hashedPassword = bcryptjs.hashSync(password, 10);
-const newUser = new User({ username, email, password: hashedPassword });
-try {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.status(201).json('User created successfully');
-
+    res.status(201).json('User created successfully!');
   } catch (error) {
     next(error);
   }
 };
-
 export const signin = async (req, res, next) => {
-const { email, password } = req.body;
-try {
-    const validUser = await User.findOne ({ email });
-    if (!validUser) return next(errorHandler(404, "We couldn't find an account that matches your email."));
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "The password you've entered is incorrect. Please try again"));
-    const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET)
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-    .cookie('access_token', token, { httpOnly: true,})
-    .status(200)
-    .json(rest);
-} catch (error) {
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
@@ -65,6 +62,15 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
   } catch (error) {
     next(error);
   }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -44,9 +44,20 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
-
 export const {
   signInStart,
   signInSuccess,
@@ -57,6 +68,9 @@ export const {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;

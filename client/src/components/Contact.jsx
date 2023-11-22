8943122633aec5856f93ae25356733499029a6cd
Japheth0214 <japarcillians@gmail,com>
2023-11-23 01:14:24 +0800

    remove the nodemailer

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({ listing }) => {
+export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [isMessageSent, setMessageSent] = useState(false);
 
   const onChange = (e) => {
     setMessage(e.target.value);
@@ -14,60 +13,27 @@ const Contact = ({ listing }) => {
   useEffect(() => {
     const fetchLandlord = async () => {
       try {
-        setLoading(true);
         const res = await fetch(`/api/user/${listing.userRef}`);
         const data = await res.json();
         setLandlord(data);
       } catch (error) {
-        console.error(error);
-        setError('Error fetching landlord information');
-      } finally {
-        setLoading(false);
+        console.log(error);
       }
     };
-
     fetchLandlord();
   }, [listing.userRef]);
 
-const handleSendMessage = async () => {
-  try {
-    const res = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        from: 'user@example.com', // The user's email
-        to: landlord.email,
-        subject: `Regarding ${listing.name}`,
-        body: message,
-      }),
-    });
-
-    const result = await res.json();
-
-    if (result.success) {
-      console.log('Email sent successfully');
-      setMessage('');
-      alert('Email sent successfully!');
-    } else {
-      console.error('Failed to send email:', result.message);
-      alert('Failed to send email. Please try again.');
-    }
-  } catch (error) {
-    console.error('Error sending email:', error.message);
-    alert('An error occurred while sending the email. Please try again.');
-  }
-};
-
+   const handleSendMessage = () => {
+    setMessageSent(true);
+    setMessage('');
+  };
   return (
     <>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
       {landlord && (
         <div className='flex flex-col gap-2'>
           <p>
-            Contact <span className='font-semibold'>{landlord.username}</span> for{' '}
+            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
+            for{' '}
             <span className='font-semibold'>{listing.name.toLowerCase()}</span>
           </p>
           <textarea
@@ -80,18 +46,20 @@ const handleSendMessage = async () => {
             className='w-full border p-3 rounded-lg'
           ></textarea>
 
-          <button
-            onClick={handleSendMessage}
-            disabled={loading}
-            className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
-          >
+         <a
+           href={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${encodeURIComponent(message)}`}
+           onClick={handleSendMessage}
+           target="_blank" rel="noopener noreferrer"
+           className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+         >
             Send Message
-          </button>
+          </a>
+           {isMessageSent && <p>Message sent successfully!</p>}
         </div>
       )}
     </>
   );
-};
+}
 
 Contact.propTypes = {
   listing: PropTypes.shape({
@@ -99,5 +67,3 @@ Contact.propTypes = {
     name: PropTypes.string.isRequired,
   }).isRequired,
 };
-
-export default Contact;
